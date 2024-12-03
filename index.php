c91cccd921694f5bb813510ee514ab67b06f8430
Noyexis <minecragolik@gmail.com>
2024-12-03 23:35:41 +0500

    start project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset='utf-8'>
+        <title>Стоматология Дельфин, г. Уфа</title>
+    </head>
+    <body>
+        <div>
+            <button onclick="SendHwaa()">HWAA</button>
+        </div>
+        <div>
+            <button onclick="initMap()">INIT</button>
+        </div>
+        <div id="map" style="width: 600px; height: 400px"></div>
+    </body>
+    <script src="./test.js"></script>
+    <script src="./jquery-3.6.0.js"></script>
+</html>
---
+++ test.js
@@ -0,0 +1,8 @@
+function SendHwaa()
+    {
+        var url = "https://api.telegram.org/bot7825856724:AAE9HhhuxDGA34oBYCQ8kWg-B9VbSBbal2E/sendMessage?chat_id=-4603636417&text=hwaaa&parse_mode=html";
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", url, true);
+        oReq.send();
+    }
+    
