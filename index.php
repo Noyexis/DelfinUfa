ad42831951fb3e1028c17cd8015749ee8aae5e96
DevArse <oneofthemoonpeople@gmail.com>
2024-12-04 00:29:59 +0500

    testovoe

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <button onclick="SendHwaa()">HWAA</button>
         </div>
         <div>
-            <button onclick="initMap()">INIT</button>
+            <button onclick="initMap()">INnig</button>
         </div>
         <div id="map" style="width: 600px; height: 400px"></div>
     </body>
