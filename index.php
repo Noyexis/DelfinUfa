97341251a78d2ed65d194a4946f39515b45b21a4
Noyexis <minecragolik@gmail.com>
2024-12-07 01:28:12 +0500

    test map tele

--- index.php
+++ index.php
@@ -2,76 +2,75 @@
 <html lang="ru">
     <head>
         <meta charset='utf-8'>
-        <link rel="stylesheet" href="./style.css">
+        <link rel="stylesheet" href="./formStyle.css">
         <title>Стоматология Дельфин, г. Уфа</title>
     </head>
     <body>
-        <div class="registerContainer">
-            <div class="registerForm">
-                <div class="title">
-                    Оставьте заявку
-                </div>
-                <div class="nameForm">
-                    <div>
-                        Ваше имя:
+        <div class="background">
+            <div class="main">
+                <div class="openRequestFormButtonContainer">
+                    <div class="openRequestFormButton backgroundBlue borderRound textCenter textWhite textNormal disableSelect">
+                        Записаться
                     </div>
-                    <input type="text" class="nameText">
                 </div>
-                <div class="dataForm">
-                    <div>
-                        Пожелания:
+                <div class="requestFormBlur">
+                    
+                </div>
+                <div class="requestFormContainer">
+                    <div class="requestForm backgroundMagenta">
+                        <div class="nameInputContainer">
+                            <input type="text" class="nameInput textWhite textSmall backgroundBlue borderRound" placeholder="Введите имя">
+                        </div>
+                        <div class="phoneInputContainer">
+                            <input type="tel" class="phoneInput textWhite textSmall backgroundBlue borderRound" placeholder="Введите номер телефона">
+                        </div>
+                        <div class="descriptionInputContainer">
+                            <textarea name="description" class="descriptionInput textWhite textSmall backgroundBlue borderRound" placeholder="Введите Ваши пожелания"></textarea>
+                        </div>
                     </div>
-                    <input type="text" class="dataText">
+                    <div class="requestFormButtons">
+                        <div class="cancelButton textWhite textSmall backgroundBlue borderRound disableSelect">
+                            Отмена
+                        </div>
+                        <div class="submitButton textWhite textSmall backgroundBlue borderRound disableSelect">
+                            Отправить
+                        </div>
+                    </div>
                 </div>
-                <div class="button" id="sendRequest">
-                    Отправить
+                <div class="mapContainer">
+                    <div id="map"></div>
                 </div>
             </div>
         </div>
-        <div id="map" style="width: 600px; height: 400px"></div>
     </body>
     <script src="https://api-maps.yandex.ru/v3/?apikey=de19fb8f-309c-4f50-ad92-2521407550e9&lang=ru_RU"></script>
     <script>
-        initMap();
-
-        async function initMap()
-      {
-          console.log(1);
-          await ymaps3.ready;
-          const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = ymaps3;
-          const map = new YMap
-          (
-            document.getElementById('map'),
+    initMap();
+    async function initMap()
+    {
+        await ymaps3.ready;
+        const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = ymaps3;
+        const map = new YMap(document.getElementById('map'),
+        {
+            location:
             {
-                location:
-                {
-                  center: [55.984784, 54.694433],
-                  zoom: 15
-                }
-            }
-           );
-           map.addChild(new YMapDefaultSchemeLayer());
-   
-          // Добавляем слой объектов
-map.addChild(new YMapDefaultFeaturesLayer())
-
-// Подключение модуля меток
-  const {YMapDefaultMarker} = (await ymaps3.import('@yandex/ymaps3-markers@0.0.1'));
-
-// Создание метки
-const myPlacemark = new YMapDefaultMarker({
-  "coordinates": [55.984784, 54.694433],
-  "color": "rgb(0, 150, 220)",
-  "title": "Дельфин",
-  "subtitle": "Стоматология"
-});
-
-// Добавление метки на карту
-map.addChild(myPlacemark);
-        
-          console.log(2);
-        };
+                center: [55.984784, 54.694433],
+                zoom: 15
+            },
+            className: "mapObject"
+        });
+        map.addChild(new YMapDefaultSchemeLayer());
+        map.addChild(new YMapDefaultFeaturesLayer());
+        const {YMapDefaultMarker} = (await ymaps3.import('@yandex/ymaps3-markers@0.0.1'));
+        const myPlacemark = new YMapDefaultMarker({
+            "coordinates": [55.984784, 54.694433],
+            "color": "rgb(0, 185, 255)",
+            "title": "Дельфин",
+            "subtitle": "Стоматология"
+        });
+        map.addChild(myPlacemark);
+    };
     </script>
     <script src="./jquery-3.6.0.js"></script>
-    <script src="./test.js"></script>
+    <script src="./form.js"></script>
 </html>
